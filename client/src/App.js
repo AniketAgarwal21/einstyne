d373ffd710e6736940a8ad8abfa7a93b78baaa5d
Aniket Agarwal <aniketggn1@gmail.com>
2021-10-23 18:17:10 +0530

    Initiated Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from "./pages/Login/Login";
+import Register from './pages/Register/Register';
 
 function App() {
-  return
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/register' exact>
+          <Register />
+        </Route>
+        <Route path='/login' exact>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ client/src/pages/Register/Register.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Register() {
+    return (
+        <section class="text-gray-400 bg-gray-900 body-font">
+            <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+                <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+                    <h1 class="title-font font-medium text-3xl text-white">Slow-carb next level shoindxgoitch ethical authentic, poko scenester</h1>
+                    <p class="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+                </div>
+                <div class="lg:w-2/6 md:w-1/2 bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+                    <h2 class="text-white text-lg font-medium title-font mb-5">Register</h2>
+                    <div class="relative mb-4">
+                        <label for="full-name" class="leading-7 text-sm text-gray-400">Full Name</label>
+                        <input type="text" id="full-name" name="full-name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-red-900 rounded border border-gray-600 focus:border-red-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
+                        <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-red-900 rounded border border-gray-600 focus:border-red-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <button class="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Register</button>
+                    <p class="text-xs mt-3">Literally you probably haven't heard of them jean shorts.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Register
