0213b994b45a2a316372c9fdcc4cc53d15ffef1c
Aniket Agarwal <aniketggn1@gmail.com>
2021-10-23 19:18:18 +0530

    deployment setup

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const http = require("http").Server(app)
 app.use(express.json())
 app.use('/', router)
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 8000
 http.listen(PORT, () => {
     console.log(`Server Running on http://localhost:${PORT}`);
