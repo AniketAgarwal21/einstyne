94ef23319e6ee4b0418d37c7de1280e48db90637
Aniket Agarwal <aniketggn1@gmail.com>
2021-10-24 11:36:15 +0530

    package.json fix

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(express.json())
 app.use('/', router)
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "client", "build")))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
-}
+// }
 
 const PORT = process.env.PORT || 8000
 http.listen(PORT, () => {
